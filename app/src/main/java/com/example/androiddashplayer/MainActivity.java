1bf2794e1fd7f07e7f3e3ce2e448e5f95ba4863f
Federico Andrucci <federico.andrucci@gmail.com>
2021-03-02 19:08:00 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/androiddashplayer/MainActivity.java
+++ app/src/main/java/com/example/androiddashplayer/MainActivity.java
@@ -8,9 +8,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        System.out.println("CIAO");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
