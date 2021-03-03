60fa32834d158ab5ffa5f58de2315e490eace409
Federico Andrucci <federico.andrucci@gmail.com>
2021-03-03 23:10:53 +0100

    test #1

--- app/src/main/java/Player/PlayerActivity.java
+++ app/src/main/java/Player/PlayerActivity.java
@@ -1,98 +1,69 @@
 package Player;
 
 import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
 public class PlayerActivity extends AppCompatActivity {
 
-    private PlayerView playerView;
-    private SimpleExoPlayer player;
-    private boolean playWhenReady = true;
-    private int currentWindow = 0;
-    private long playbackPosition = 0;
+    SimpleExoPlayer exoPlayer;
+    PlayerView exoPlayerView;
+    String videoURL = "http://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        System.out.println("Ciao");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        playerView = findViewById(R.id.video_view);
-    }
+        exoPlayerView = findViewById(R.id.idExoPlayerVIew);
 
-    private void initializePlayer() {
-        player = new SimpleExoPlayer.Builder(this).build();
-        playerView.setPlayer(player);
+        try {
+            DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory();
+            MediaSource mediaSource =
+                    new DashMediaSource.Factory(dataSourceFactory)
+                            .createMediaSource(MediaItem.fromUri(videoURL));
+            SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build();
+            player.setMediaSource(mediaSource);
+            player.prepare();
+            /*
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory());
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+            Uri videouri = Uri.parse(videoURL);
 
-        MediaItem mediaItem = MediaItem.fromUri("https://storage.googleapis.com/exoplayer-test-media-0/play.mp3");
-        player.setMediaItem(mediaItem);
+            DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_video");
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+            MediaSource mediaSource = new ExtractorMediaSource(videouri, dataSourceFactory, extractorsFactory, null, null);
+            exoPlayerView.setPlayer(exoPlayer);
 
-        player.setPlayWhenReady(playWhenReady);
-        player.seekTo(currentWindow, playbackPosition);
-        player.prepare();
-    }
+            exoPlayer.prepare(mediaSource);
 
-    private void releasePlayer() {
-        if(player != null) {
-            playbackPosition = player.getCurrentPosition();
-            currentWindow = player.getCurrentWindowIndex();
-            playWhenReady = player.getPlayWhenReady();
-            player.release();
-            player = null;
+            exoPlayer.setPlayWhenReady(true);
+*/
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (Util.SDK_INT > 23) {
-            initializePlayer();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        hideSystemUi();
-        if ((Util.SDK_INT <= 23 || player == null)) {
-            initializePlayer();
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (Util.SDK_INT <= 23) {
-            releasePlayer();
-        }
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (Util.SDK_INT > 23) {
-            releasePlayer();
-        }
-    }
-
-    @SuppressLint("InlinedApi")
-    private void hideSystemUi() {
-        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
 
