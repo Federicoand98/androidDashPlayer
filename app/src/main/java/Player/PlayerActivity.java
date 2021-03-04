fe8bc35a450ad9c4e9f02ea7266ec52fe45afdcd
Federico Andrucci <federico.andrucci@gmail.com>
2021-03-05 00:17:13 +0100

    dash

--- app/src/main/java/Player/PlayerActivity.java
+++ app/src/main/java/Player/PlayerActivity.java
@@ -14,6 +14,7 @@ import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.audio.MediaCodecAudioRenderer;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
@@ -31,6 +32,7 @@ import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Log;
 import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.android.exoplayer2.util.Util;
 import com.google.android.exoplayer2.video.MediaCodecVideoRenderer;
@@ -40,42 +42,25 @@ import java.net.URL;
 
 public class PlayerActivity extends AppCompatActivity {
 
-    SimpleExoPlayer exoPlayer;
-    PlayerView exoPlayerView;
     String videoURL = "http://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd";
-    String video = "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4";
+
+    private PlaybackStateListener playbackStateListener;
+    private static final String TAG = PlayerActivity.class.getName();
+
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        exoPlayerView = findViewById(R.id.idExoPlayerVIew);
+        playerView = findViewById(R.id.idExoPlayerVIew);
 
-        exoPlayer = ExoPlayerFactory.newSimpleInstance(this);
-
-/*
-        try {
-
-            DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory();
-            //MediaSource mediaSource = new DashMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(Uri.parse(video)));
-            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-            MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(videoURL), dataSourceFactory, extractorsFactory, null, null);
-
-            TrackSelection.Factory adaptiveTrackSelection = new AdaptiveTrackSelection.Factory();
-            TrackSelector trackSelector = new DefaultTrackSelector(adaptiveTrackSelection);
-
-            DefaultLoadControl loadControl = new DefaultLoadControl.Builder().setBufferDurationsMs(64 * 1024, 128 * 1024, 1024, 1024).createDefaultLoadControl();
-
-            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
-            exoPlayerView.setPlayer(exoPlayer);
-
-            exoPlayer.prepare(mediaSource);
-            exoPlayer.setPlayWhenReady(true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
- */
+        playbackStateListener = new PlaybackStateListener();
     }
 
     @Override
@@ -90,7 +75,7 @@ public class PlayerActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         hideSystemUi();
-        if ((Util.SDK_INT <= 23 || exoPlayer == null)) {
+        if ((Util.SDK_INT <= 23 || player == null)) {
             initializePlayer();
         }
     }
@@ -112,21 +97,42 @@ public class PlayerActivity extends AppCompatActivity {
     }
 
     private void initializePlayer() {
-        if(exoPlayer == null) {
+        if (player == null) {
             DefaultTrackSelector trackSelector = new DefaultTrackSelector(this);
-            trackSelector.setParameters(trackSelector.buildUponParameters().setMaxVideoSizeSd());
+            trackSelector.setParameters(
+                    trackSelector.buildUponParameters().setMaxVideoSizeSd());
+            player = new SimpleExoPlayer.Builder(this)
+                    .setTrackSelector(trackSelector)
+                    .build();
+        }
 
-            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        playerView.setPlayer(player);
+        MediaItem mediaItem = new MediaItem.Builder()
+                .setUri(getString(R.string.mpd_test))
+                .setMimeType(MimeTypes.APPLICATION_MPD)
+                .build();
+        player.setMediaItem(mediaItem);
 
-            MediaItem mediaItem = new MediaItem.Builder().setUri(Uri.parse(videoURL)).setMimeType(MimeTypes.APPLICATION_MPD).build();
-            exoPlayer.setMediaItem(mediaItem);
-            exoPlayer.prepare();
-            exoPlayer.setPlayWhenReady(true);
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        player.addListener(playbackStateListener);
+        player.prepare();
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.removeListener(playbackStateListener);
+            player.release();
+            player = null;
         }
     }
 
+    @SuppressLint("InlinedApi")
     private void hideSystemUi() {
-        exoPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -134,8 +140,30 @@ public class PlayerActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
-    private void releasePlayer() {
-        exoPlayer.release();
+    private class PlaybackStateListener implements Player.EventListener {
+
+        @Override
+        public void onPlaybackStateChanged(int playbackState) {
+            String stateString;
+            switch (playbackState) {
+                case ExoPlayer.STATE_IDLE:
+                    stateString = "ExoPlayer.STATE_IDLE      -";
+                    break;
+                case ExoPlayer.STATE_BUFFERING:
+                    stateString = "ExoPlayer.STATE_BUFFERING -";
+                    break;
+                case ExoPlayer.STATE_READY:
+                    stateString = "ExoPlayer.STATE_READY     -";
+                    break;
+                case ExoPlayer.STATE_ENDED:
+                    stateString = "ExoPlayer.STATE_ENDED     -";
+                    break;
+                default:
+                    stateString = "UNKNOWN_STATE             -";
+                    break;
+            }
+            Log.d(TAG, "changed state to " + stateString);
+        }
     }
 }
 
