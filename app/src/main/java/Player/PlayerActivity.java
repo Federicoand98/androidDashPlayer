7b901ea7e4531aef84637b0f43de5c3c125699a5
Federico Andrucci <federico.andrucci@gmail.com>
2021-03-04 22:40:24 +0100

    a

--- app/src/main/java/Player/PlayerActivity.java
+++ app/src/main/java/Player/PlayerActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -17,19 +20,27 @@ import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
+
+import java.net.URI;
+import java.net.URL;
 
 public class PlayerActivity extends AppCompatActivity {
 
     SimpleExoPlayer exoPlayer;
     PlayerView exoPlayerView;
     String videoURL = "http://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd";
+    String video = "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4";
+
+    https://github.com/google/ExoPlayer/blob/release-v2/demos/main/src/main/java/com/google/android/exoplayer2/demo/PlayerActivity.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,28 +50,22 @@ public class PlayerActivity extends AppCompatActivity {
         exoPlayerView = findViewById(R.id.idExoPlayerVIew);
 
         try {
-            DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory();
-            MediaSource mediaSource =
-                    new DashMediaSource.Factory(dataSourceFactory)
-                            .createMediaSource(MediaItem.fromUri(videoURL));
-            SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build();
-            player.setMediaSource(mediaSource);
-            player.prepare();
-            /*
-            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-            TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory());
-            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
-            Uri videouri = Uri.parse(videoURL);
 
-            DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_video");
+            DataSource.Factory dataSourceFactory = new DefaultHttpDataSourceFactory();
+            //MediaSource mediaSource = new DashMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(Uri.parse(video)));
             ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-            MediaSource mediaSource = new ExtractorMediaSource(videouri, dataSourceFactory, extractorsFactory, null, null);
+            MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(videoURL), dataSourceFactory, extractorsFactory, null, null);
+
+            TrackSelection.Factory adaptiveTrackSelection = new AdaptiveTrackSelection.Factory();
+            TrackSelector trackSelector = new DefaultTrackSelector(adaptiveTrackSelection);
+
+            DefaultLoadControl loadControl = new DefaultLoadControl.Builder().setBufferDurationsMs(64 * 1024, 128 * 1024, 1024, 1024).createDefaultLoadControl();
+
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
             exoPlayerView.setPlayer(exoPlayer);
 
             exoPlayer.prepare(mediaSource);
-
             exoPlayer.setPlayWhenReady(true);
-*/
         } catch (Exception e) {
             e.printStackTrace();
         }
